f761e51d6dedc49b5e0e9333f06226061994dba9
Harikrishnan V K <85347203+hk-vk@users.noreply.github.com>
2024-11-01 23:03:16 +0530

    fixed pages.js

--- app/commitdata/page.js
+++ app/commitdata/page.js
@@ -1,7 +1,10 @@
-// import { ComicCommitTimeline } from "@/@/components/comic-commit-timeline";
-// export default function ComicCommitTimelinePage() {
-//     return (
-//         <ComicCommitTimeline  />
-//     )
-//     }
-    
+//add some sample cod
+import React from 'react';
+export default function GitStoryGeneratorPage() {
+    return (
+        <div>
+            <h1>Git Story Generator</h1>
+            <p>Generate a story from your git commits!</p>
+        </div>
+    )
+}
