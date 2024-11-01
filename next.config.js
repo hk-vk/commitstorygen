f7257b9c2afce85693dbd27bbe97e4d190012076
Harikrishnan V K <85347203+hk-vk@users.noreply.github.com>
2024-11-01 23:00:51 +0530

    updated next config .js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+
 module.exports = {
     eslint: {
       // Allow builds to succeed even if there are ESLint errors
       ignoreDuringBuilds: true,
     },
+    webpack: (config) => {
+      config.resolve.alias['@'] = path.join(__dirname); // Adjust based on your folder structure
+      return config;
+    },
   };
+
